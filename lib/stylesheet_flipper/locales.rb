0b52655df778bd6360a1e5fd4fa6163cafe56358
Jeppe Liisberg <jeppe@liisberg.net>
2013-12-04 20:36:46 +0100

    Avoid implicit parentheses

--- lib/stylesheet_flipper/locales.rb
+++ lib/stylesheet_flipper/locales.rb
@@ -2,10 +2,10 @@ module StylesheetFlipper
   def self.flipped_locales
     @flipped_locales ||= [:ar, :ckb, :fa, :he, :ug]
   end
-  
+
   def self.flipped_locales=(value)
     raise ArgumentError.new('flipped_locales should be an array') unless value.is_a?(Array)
-    value.map! &:to_sym
+    value.map!(&:to_sym)
     @flipped_locales = value
   end
 end
