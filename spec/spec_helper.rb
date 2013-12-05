10ba43f87a1c0f13208e1bb7d73b7369ec3eaa08
Jeppe Liisberg <jeppe@liisberg.net>
2013-12-05 19:28:49 +0100

    Use instance variables in specs as well
    No need for class variables in there either (see 2a23bb9c9b5e1a9).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 # mock up rails' I18n module
 module I18n
   def self.locale
-    @@locale ||= :en
+    @locale ||= :en
   end
 
   def self.locale=(value)
-    @@locale = value
+    @locale = value
   end
 end
